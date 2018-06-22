fb14411a633ab5b731a88dcca55a43c63dbcdded
Mark T. Holder <mtholder@gmail.com>
2018-06-22 11:21:26 -0500

    closer to an actual test

--- otwstest/__init__.py
+++ otwstest/__init__.py
@@ -1,21 +1,49 @@
-import os
-import sys
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
-SYST_CHOICES = frozenset(['production', 'dev', 'local'])
-SCRIPT_DIR = os.path.abspath(os.path.dirname(sys.argv[0]))
-NUM_FAILURES = 0
+import sys
+import types
+
+from . import taxonomy
+
+
+def run_test(test_config, test_addr, test_func, test_results):
+    """Core"""
+    try:
+        if test_func(test_config, test_addr, test_results):
+            return True
+    except Exception:
+        test_results.flag_uncaught(test_addr)
+    return False
+
+def run_tests(test_config, addr_fn_pairs_list, test_results):
+    good = True
+    for test_addr, fn in addr_fn_pairs_list:
+        good = run_test(test_config,
+                        test_addr=test_addr,
+                        test_func=fn,
+                        test_results=test_results) and good
+    return good
+
 
 
 class TestResults(object):
     def __init__(self):
-        self.num_tests_run = 0
-        self.num_tests_skipped = 0
-        self.num_failures = 0
-        self.num_errors = 0
-        self.num_exceptions_not_caught_by_test = 0
+        self._run = []
+        self._skipped = []
+        self._failures = []
+        self._errors = []
+        self._exceptions_uncaught = []
 
-    def flag_uncaught(self):
-        self.num_exceptions_not_caught_by_test += 1
+    def flag_uncaught(self, addr):
+        self._exceptions_uncaught.append(addr)
+
+    @property
+    def num_problems(self):
+        return sum([len(i) for i in (self._failures, self._errors, self._exceptions_uncaught)])
+
+
+SYST_CHOICES = frozenset(['production', 'dev', 'local'])
 
 
 class TestingConfig(object):
@@ -31,32 +59,24 @@ class TestingConfig(object):
         return a
 
 
-def run_test(test_config, test_func, test_results):
-    try:
-        if test_func(test_config, test_results):
-            return True
-    except Exception:
-        test_results.flag_uncaught()
-    return False
+def _collect_file_func_pairs(mod_obj, addr):
+    ret = []
+    for k, v in mod_obj.__dict__.items():
+        if k.startswith('_'):
+            continue
+        extended = '{}.{}'.format(addr, k)
+        if k.startswith('test'):
+            ret.append((extended, v))
+        elif isinstance(v, types.ModuleType):
+            ret.extend(_collect_file_func_pairs(v, extended))
+    return ret
 
 
-def run_tests(test_config, srcfile_fn_pairs_list, test_results):
-    good = True
-    for srcfn, fn in srcfile_fn_pairs_list:
-        good = run_test(test_config, test_func=fn, test_results=test_results) and good
-    return good
-
-
-def delegated_main(args, src_file, test_fn):
-    top_main(args, deleg_file=src_file, deleg_fn=test_fn)
-
-
-def top_main(argv, deleg_file=None, deleg_fn=None):
-    global NUM_FAILURES
+def top_main(argv, deleg=None):
     import argparse
     description = "Tests of web services for Open Tree of Life project"
-    if deleg_file is not None:
-        description += ' tests just from "{}"'.format(deleg_file)
+    if deleg is not None:
+        description += ' tests just from "{}"'.format(deleg)
     p = argparse.ArgumentParser(description=description)
     p.add_argument("--show-completions",
                    action="store_true",
@@ -69,7 +89,7 @@ def top_main(argv, deleg_file=None, deleg_fn=None):
 
     p.add_argument('--system', choices=SYST_CHOICES, default='production')
     serv_choices = ('taxonomy',)
-    if deleg_file is None:
+    if deleg is None:
         p.add_argument('service', nargs='?', choices=serv_choices)
     tr = TestResults()
     if "--show-completions" in argv:
@@ -90,33 +110,25 @@ def top_main(argv, deleg_file=None, deleg_fn=None):
                 elif syst_ind + 2 >= len(a):
                     tc_syst_choices = SYST_CHOICES
         comp_list.extend(tc_syst_choices)
-        if deleg_file is None:
+        if deleg is None:
             if a[-1] != '=' and not a[-1].startswith('-'):
                 for s in serv_choices:
                     if s not in a:
                         comp_list.append(s)
         sys.stdout.write('{}\n'.format(' '.join(comp_list)))
         # sys.stderr.write('\n{}\n'.format(' '.join(comp_list)))
-    else:
-        parsed = p.parse_args(args=argv[1:])
-        tc = TestingConfig(system_to_test=parsed.system,
-                           verbose=parsed.verbose)
-        '''
-        if deleg_file is None:
-            services = list(parsed.service or serv_choices)
-            services.sort()
-            for s in services:
-                for top_dir, sub_d_list, filename_list in os.walk(os.path.join(SCRIPT_DIR, s)):
-                    for fn in filename_list:
-                        if fn.startswith('test_') and fn.endswith('.py'):
-                            fp = os.path.join(top_dir, fn)
-                            print('fp="{}"'.format(fp))
-                            invoc = [sys.executable, fp] + tc.as_arg_list()
-                            print('invoc = {}'.format(invoc))
-                            rc = subprocess.call(invoc)
-                            NUM_FAILURES += rc
-        else:
-            file_fn_list = [(deleg_file, deleg_fn)]
-            run_tests(tc, file_fn_list)
-        '''
+        return tr
+    parsed = p.parse_args(args=argv[1:])
+    tc = TestingConfig(system_to_test=parsed.system,
+                       verbose=parsed.verbose)
+    if deleg is None:
+        # noinspection PyUnresolvedReferences
+        import otwstest
+        services = list(parsed.service or serv_choices)
+        services.sort()
+        addr = 'otwstest.'
+        file_func_pairs = []
+        for s in services:
+            file_func_pairs.extend(_collect_file_func_pairs(otwstest.__dict__[s], addr + s))
+        run_tests(tc, file_func_pairs, tr)
     return tr

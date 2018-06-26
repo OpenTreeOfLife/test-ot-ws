4cc93f181a2ef4d2a5852261c46d0dea8e72b3d7
Mark T. Holder <mtholder@gmail.com>
2018-06-25 20:59:40 -0500

    lica test passing that probably should fail

--- otwstest/taxonomy/lica/basic.py
+++ otwstest/taxonomy/lica/basic.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python
 from otwstest.schema.taxonomy.lica import validate
 
-def tests(config, outcome):
+def testsimple(config, outcome):
     url = config.make_url('v2/taxonomy/lica')
     outcome.do_http_json(url, 'POST', data={"ott_ids":[515698,590452,409712,643717]},
                          expected_status=200, validator=lambda x : validate(x, 'v2'))
+
+def testnoarg(config, outcome):
+    url = config.make_url('v2/taxonomy/lica')
+    outcome.do_http_json(url, 'POST', data={"ott_ids": []},
+                         expected_status=200, validator=lambda x: validate(x, 'v2'))
+

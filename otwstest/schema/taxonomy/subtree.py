cb807d78e875c8adda14d77104e2bf205f68a103
Mark T. Holder <mtholder@gmail.com>
2018-06-28 04:54:36 -0500

    basic subtree test

---
+++ otwstest/schema/taxonomy/subtree.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import copy
+import jsonschema
+
+current = {
+  "$id": "https://tree.opentreeoflife.org/schema/current/taxonomy/flags.json",
+  "type": "object",
+  "$schema": "http://json-schema.org/draft-07/schema#",
+  "properties": {
+    "subtree": {"type": "string"}
+  },
+  "required": ["subtree"]
+}
+
+v3 = copy.deepcopy(current)
+v3['$id'] = v3['$id'].replace('/current/', '/v3/')
+v2 = copy.deepcopy(current)
+v2['$id'] = v2['$id'].replace('/current/', '/v3/')
+
+_version2schema = {'current': current, 'v2': v2, 'v3': v3}
+
+def validate(doc, version='current'):
+    schema = _version2schema[version]
+    jsonschema.validate(doc, schema)
+    for p in schema["required"]:
+        if doc[p] < 0:
+            m = 'Expecting "{}" field to be non-negative, found "{}"'.format(p, doc[p])
+            raise jsonschema.ValidationError(m)
+    return True
+

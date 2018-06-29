841cd10d48132e0bc712ad995e6bbd802dee4447
Mark T. Holder <mtholder@gmail.com>
2018-06-29 05:47:40 -0500

    taxon test

--- otwstest/taxonomy/taxon/__init__.py
+++ otwstest/taxonomy/taxon/__init__.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from otwstest.schema.taxonomy.subtree import validate
+from otwstest.schema.taxonomy.taxon import validate
 import re
 
 
@@ -8,14 +8,10 @@ def test_simple(config, outcome):
     url = config.make_url('v2/taxonomy/taxon')
     result = outcome.do_http_json(url, 'POST', data={"ott_id": 901642},
                                   expected_status=200, validator=lambda x: validate(x, 'v2'))
-    # TEST_NAME = u'Alseuosmia banksii'
-    # if u'ot:ottTaxonName' not in result:
-    #     sys.stderr.write('ot:ottTaxonName not returned in result\n')
-    #     sys.exit(1)
-    # taxonName = result[u'ot:ottTaxonName']
-    # if taxonName != TEST_NAME:
-    #     errstr = 'Expected taxon name {} but not found in \n{}\n'
-    #     sys.stderr.write(errstr.format(TEST_NAME,taxonName))
-    #     sys.exit(1)
+    expected = u'Alseuosmia banksii'
+    obs = result[u'ot:ottTaxonName']
+    if obs != expected:
+         errstr = 'Expected taxon name "{}", but found in "{}"'.format(expected, obs)
+         outcome.exit_test_with_failure(errstr)
 
 

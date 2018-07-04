058b2fc3be303aaaec218218b2e72b0156207756
Mark T. Holder <mtholder@gmail.com>
2018-07-04 12:40:27 -0500

    tnrs/contexts

--- otwstest/tnrs/__init__.py
+++ otwstest/tnrs/__init__.py
@@ -4,6 +4,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import re
+from otwstest import is_str_type
 import otwstest.schema.tnrs as tnrs
 
 def test_autocomplete_name(config, outcome):  #taxonomy-sensitive test
@@ -22,13 +23,19 @@ def test_autocomplete_name(config, outcome):  #taxonomy-sensitive test
 def test_contexts(config, outcome):  #taxonomy-sensitive test
     url = config.make_url('v2/tnrs/contexts')
     search_name = "Endoxyla"
-    result = outcome.do_http_json(url, 'POST',
-                                  validator=lambda x: tnrs.autocomplete_name.validate(x, 'v2'))
-    for res in result:
-        uname = res["unique_name"]
-        if not re.search(search_name, uname):
-            errstr = 'unique_name: "{}" of taxon record does not contain search string "{}"'
-            outcome.exit_test_with_failure(errstr.format(uname, search_name))
+    result = outcome.do_http_json(url, 'POST')
+    for top, sub in result.items():
+        if not is_str_type(top):
+            errstr = 'expecting keys of tnrs/contexts to be strings found {}'
+            outcome.exit_test_with_failure(errstr.format(repr(top)))
+        if not isinstance(sub, list):
+            errstr = 'expecting values of tnrs/contexts to be a list of strings found {}'
+            outcome.exit_test_with_failure(errstr.format(repr(sub)))
+        for s in sub:
+            if not is_str_type(s):
+                errstr = 'expecting values of tnrs/contexts to be a list of strings found {}'
+                outcome.exit_test_with_failure(errstr.format(repr(s)))
+
 
 
 

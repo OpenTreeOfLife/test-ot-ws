2700ec76e81b0c679fcca5fb05438bf47a2511e3
Mark T. Holder <mtholder@gmail.com>
2018-08-07 11:26:34 -0500

    file_get

--- otwstest/studies/__init__.py
+++ otwstest/studies/__init__.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from . import (find_studies, find_trees,
+from . import (file_get, find_studies, find_trees,
                properties,
                study_get, study_list,
                tree_get,)
---
+++ otwstest/studies/file_get/__init__.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from otwstest import all_api_versions
+import jsonschema
+from otwstest.schema.primitives import SCHEMA_ARRAY_OF_STRINGS
+
+
+@all_api_versions
+def test_file(outcome):
+    study_id = 'ot_134'
+    url = outcome.make_url('study/{}/file'.format(study_id))
+    x = outcome.do_http_json(url)
+    if len(x) == 0:
+        m = 'expecting study {} to have an attached file.'.format(study_id)
+        outcome.exit_test_with_failure(m)
+    file_id = x[0]['id']
+    url = outcome.make_url('study/{}/file/{}'.format(study_id, file_id))
+    outcome.do_http_json(url, return_raw_content=True)
+    

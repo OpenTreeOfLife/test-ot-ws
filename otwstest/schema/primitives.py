99cb9ee8ace85b57106bee8c407a60e61317eb60
Mark T. Holder <mtholder@gmail.com>
2018-08-07 10:23:12 -0500

    suppressing pep8 warnings for schema type abbreviations

--- otwstest/schema/primitives.py
+++ otwstest/schema/primitives.py
@@ -1,10 +1,37 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-SCHEMA_STRING = lambda: {"type": "string"}
-SCHEMA_INTEGER = lambda: {"type": "integer"}
-SCHEMA_BOOL = lambda: {"type": "boolean"}
-SCHEMA_OBJECT = lambda: {"type": "object"}
-SCHEMA_ARRAY_OF_STRINGS = lambda: {"type": "array", "items": SCHEMA_STRING()}
-SCHEMA_ARRAY = lambda: {"type": "array"}
-SCHEMA_ARRAY_OBJECTS = lambda: {"type": "array", "items": SCHEMA_OBJECT()}
+
+# noinspection PyPep8Naming
+def SCHEMA_STRING():
+    return {"type": "string"}
+
+
+# noinspection PyPep8Naming
+def SCHEMA_INTEGER():
+    return {"type": "integer"}
+
+
+# noinspection PyPep8Naming
+def SCHEMA_BOOL():
+    return {"type": "boolean"}
+
+
+# noinspection PyPep8Naming
+def SCHEMA_OBJECT():
+    return {"type": "object"}
+
+
+# noinspection PyPep8Naming
+def SCHEMA_ARRAY_OF_STRINGS():
+    return {"type": "array", "items": SCHEMA_STRING()}
+
+
+# noinspection PyPep8Naming
+def SCHEMA_ARRAY():
+    return {"type": "array"}
+
+
+# noinspection PyPep8Naming
+def SCHEMA_ARRAY_OBJECTS():
+    return {"type": "array", "items": SCHEMA_OBJECT()}
